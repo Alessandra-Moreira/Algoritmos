181d13a48db4e03f90182cca343b7c8e2e635058
Alessandra Moreira <alessandra.moreira01@fatec.gov.sp.br>
2023-03-16 10:30:10 -0300

    lista V

--- lista_exercicios_IV.py
+++ lista_exercicios_IV.py
@@ -33,7 +33,9 @@ print(f"Lista: {n}")
 print(f"Pares: {par}")
 print(f"Impares: {impar}")
 
-# 3
+# 3 Faça um programa que crie dois vetores com 10 elementos aleatórios entre 1 e 100. Gere um
+# terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos
+# intercalados dos dois outros vetores. Imprima os três vetores.
 
 vetor1 = random.sample(range(100), 10)
 vetor2 = random.sample(range(100), 10)
@@ -50,7 +52,10 @@ print(vetor1)
 print(vetor2)
 print(vetor3)
 
-# 4
+# 4 com o texto a seguir,  Gere uma lista de palavras deste texto com
+# split(), a seguir crie uma lista com as palavras que começam ou terminam com uma das letras
+# “python”. Imprima a lista resultante. Não se esqueça de remover antes os caracteres especiais
+# e cuidado com maiúsculas e minúsculas.
 text = '''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. 
 Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles.
 We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''
@@ -66,7 +71,9 @@ for p in new_text:
 
 print(lista)
 
-# 5
+# 5 Seja o mesmo texto acima “splitado”. Calcule quantas palavras possuem uma das letras
+# “python” e que tenham mais de 4 caracteres. Não se esqueça de transformar maiúsculas para
+# minúsculas e de remover antes os caracteres especiais
 text = '''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. 
 Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles.
 We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''
@@ -75,12 +82,14 @@ text = text.replace(":", "").replace(",", "").replace(".", "")
 text = text.lower()
 new_text = text.split()
 
+
 def search(palavra):
     for letra in palavra:
         if letra in "python":
             return True
     return False
-        
+
+
 lista = []
 for p in new_text:
     if search(p) and len(p) > 4:
